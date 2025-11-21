095c3da93847da80fb10c7ad9270a4a06e42fdf4
Xavier <xavier.sotomayor21@hotmail.com>
2025-11-20 21:28:15 -0600

    Estructura base ejercicios 1.6 en delate

--- Part1/exercise1/src/App.jsx
+++ Part1/exercise1/src/App.jsx
@@ -1,44 +1,14 @@
 import { useState } from 'react'
 
-const History = (props) => {
-
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  
-  return (
-    <div>
-      History of clicks → {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
-const Button = ({ handleClick, text }) => (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-)
-
-const Display = props => <div>{props.value}</div>
-
 const App = () => {
-  const [value, setValue] = useState(10)
-
-  const setToValue = newValue => {
-    console.log('value now', newValue)
-    setValue(newValue)
-  }
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Display value={value} />
-      <Button handleClick={() => setToValue(1000)} text="thousand" />
-      <Button handleClick={() => setToValue(0)} text="reset" />
-      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+      code here
     </div>
   )
 }
