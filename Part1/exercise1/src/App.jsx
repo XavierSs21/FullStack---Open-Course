f195507b4b04033271ea146580c8379e8317ec23
Xavier <xavier.sotomayor21@hotmail.com>
2025-11-22 14:24:15 -0600

    exercise 1.14 - Agregar anecdota con mas votos y su resultado

--- Part1/exercise1/src/App.jsx
+++ Part1/exercise1/src/App.jsx
@@ -4,7 +4,26 @@ const Header = ({ text }) => <h1>{text}</h1>
 
 const Button = ({ handleEvent, text }) => <button onClick={handleEvent}>{text}</button>
 
+const MaxAnecdote = ({ votes, anecdote }) => {
+  const maxVotes = Math.max(...votes)
+  
+  if(maxVotes === 0)
+    return <div>no votes given to any anecdote</div>
+  
+  const maxAnecdote = votes.indexOf(maxVotes)
+
+  return (
+    <div>
+      <p>{anecdote[maxAnecdote]}</p>
+      <p>has {maxVotes} {maxVotes === 1 ? 'vote' : 'votes'}</p>
+    </div>
+  )
+}
+
 const App = () => {
+
+  const header = 'Anecdote of the day'
+  const topAnecdote = 'Anecdote with most votes'
   const anecdotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
@@ -37,10 +56,13 @@ const App = () => {
 
   return (
     <div>
-      <Header text={anecdotes[selected]}/>
+      <Header text={header}/>
+      <p>{anecdotes[selected]}</p>
       <p>has {votes[selected]} {votes[selected] === 1 ? 'vote': 'votes'}</p>
       <Button handleEvent={handleVotes} text={'vote'}/>
       <Button handleEvent={handleAnecdote} text={'next anecdote'}/>
+      <Header text={topAnecdote}/>
+      <MaxAnecdote votes={votes} anecdote={anecdotes}/>
     </div>
   )
 }
